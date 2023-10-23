5f1cf4d57fb3ae68a41b623d03c24bcaead3f5a5
jake-is-ESD-protected <jt@tschavoll.at>
2023-10-23 23:28:57 +0200

    empty files for mini CLI

---
+++ lib/mini_cli/mini_cli.cpp
@@ -0,0 +1,1 @@
+#include "mini_cli.h"
---
+++ lib/mini_cli/mini_cli.h
@@ -0,0 +1,7 @@
+#ifndef _MINI_CLI_H_
+#define _MINI_CLI_H_
+
+/// @file mini_cli.h headers for the internal mini CLI via UART connection. 
+/// Used for unit tests, debugging and data transfer.
+
+#endif
