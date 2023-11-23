01aaadd618af4526c1a8b75caf6ad1f48956700c
jake-is-ESD-protected <jt@tschavoll.at>
2023-11-23 11:39:57 +0100

    add mem related macros

--- src/mem/mem.hpp
+++ src/mem/mem.hpp
@@ -1,4 +1,6 @@
-#pragma once
+// #pragma once
+#ifndef _MEM_HPP_
+#define _MEM_HPP_
 
 #include "daisy_seed.h"
 
@@ -6,7 +8,11 @@
 #define MEM_BLOCK_OK 1
 #define MEM_FINISH 2
 
-#define BLOCKSIZE 1024
+#define SDRAM_BUF_SIZE 65536
+#define BLOCK_PREFIX_LEN 4
+#define BLOCK_POSTFIX_LEN 4
+#define BLOCKSIZE_RAW 64
+#define BLOCKSIZE_DATA (BLOCKSIZE_RAW - BLOCK_PREFIX_LEN)
 
 using namespace daisy;
 
@@ -15,3 +21,7 @@ void memInit(DaisySeed *_pHw);
 uint8_t memSdramWrite(void *buf, uint32_t len);
 
 uint8_t memQspiWrite(void *buf, uint32_t len);
+
+bool isEOF(void *buf);
+
+#endif
