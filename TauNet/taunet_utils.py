928a2f1aaf2581189dd30298fb976a26a345767c
jake-is-ESD-protected <jt@tschavoll.at>
2024-04-20 19:26:03 +0200

    update utils

--- TauNet/taunet_utils.py
+++ TauNet/taunet_utils.py
@@ -2,6 +2,21 @@ import os
 import scipy as sp
 import numpy as np
 
+def read_audio_file(path, truncate_at=10):
+    if path.endswith(".wav"):  # Check for .wav files
+        sample_rate, audio_data = sp.io.wavfile.read(path)
+        try:
+            audio_data = audio_data[:, 0]
+        except:
+            pass
+        n_bits = 32  # Assuming 32-bit audio
+        audio_data = audio_data / (2**(n_bits - 1))  # Adjust range to -1 to 1 
+        audio_data /= np.abs(np.max(audio_data))  # Safer normalization
+        audio_data = audio_data[: sample_rate * truncate_at]
+        label = os.path.splitext(os.path.basename(path))[0]  # Extract filename without extension
+        return (audio_data, label, sample_rate)
+
+
 def read_audio_files(directory, truncate_at=10):
     """
     Reads all audio files from a directory and returns their data with file name labels.
@@ -17,18 +32,7 @@ def read_audio_files(directory, truncate_at=10):
     """
     audio_data_list = []
     for filename in os.listdir(directory):
-        if filename.endswith(".wav"):  # Check for .wav files
-            filepath = os.path.join(directory, filename)
-            sample_rate, audio_data = sp.io.wavfile.read(filepath)
-            try:
-                audio_data = audio_data[:, 0]
-            except:
-                pass
-            n_bits = 32  # Assuming 32-bit audio
-            audio_data = audio_data / (2**(n_bits - 1))  # Adjust range to -1 to 1 
-            audio_data /= np.abs(np.max(audio_data))  # Safer normalization
-            audio_data = audio_data[: sample_rate * truncate_at]
-            label = os.path.splitext(filename)[0]  # Extract filename without extension
-            audio_data_list.append((audio_data, label, sample_rate))
-
+        audio = read_audio_file(os.path.join(directory, filename))
+        if audio:
+            audio_data_list.append(audio)
     return audio_data_list
