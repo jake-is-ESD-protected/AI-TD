e7b27de4ae8c787e8ea05ccd121bc10fcf1b0df5
KristofKonya <kristofvonkonya@hotmail.de>
2024-05-27 15:25:23 +0200

    end of session the earth is flat now

--- src/ai/ai.cpp
+++ src/ai/ai.cpp
@@ -3,42 +3,72 @@
 #include "model.h"
 #include <vector>
 
-// clang-format off
+#define LOAD_WEIGHTS(SHAPE_IN, SHAPE_OUT, target, source, counter)                                                                                                                                                                                                                                                                                                                                             \
+    do                                                                                                                                                                                                                                                                                                                                                                                                         \
+    {                                                                                                                                                                                                                                                                                                                                                                                                          \
+        uint32_t *sourcePointer = source + counter;                                                                                                                                                                                                                                                                                                                                                            \
+        for (int i = 0; i < SHAPE_OUT; i++)                                                                                                                                                                                                                                                                                                                                                                    \
+        {                                                                                                                                                                                                                                                                                                                                                                                                      \
+            std::vector<float> row;                                                                                                                                                                                                                                                                                                                                                                            \
+            for (int j = 0; j < SHAPE_IN; j++)                                                                                                                                                                                                                                                                                                                                                                 \
+            {                                                                                                                                                                                                                                                                                                                                                                                                  \
+                row.push_back(*(float *)&sourcePointer[i * SHAPE_IN + j]);                                                                                                                                                                                                                                                                                                                                     \
+            }                                                                                                                                                                                                                                                                                                                                                                                                  \
+            target.push_back(row);                                                                                                                                                                                                                                                                                                                                                                             \
+        }                                                                                                                                                                                                                                                                                                                                                                                                      \
+        counter += SHAPE_IN * SHAPE_OUT;                                                                                                                                                                                                                                                                                                                                                                       \
+    } while (0)
+
+#define LOAD_BIAS(SHAPE_OUT, target, source, counter)                                                                                                                                                                                                                                                                                                                                                          \
+    do                                                                                                                                                                                                                                                                                                                                                                                                         \
+    {                                                                                                                                                                                                                                                                                                                                                                                                          \
+        uint32_t *sourcePointer = source + counter;                                                                                                                                                                                                                                                                                                                                                            \
+        for (int i = 0; i < SHAPE_OUT; i++)                                                                                                                                                                                                                                                                                                                                                                    \
+        {                                                                                                                                                                                                                                                                                                                                                                                                      \
+            target[i] = *(float *)&sourcePointer[i];                                                                                                                                                                                                                                                                                                                                                           \
+        }                                                                                                                                                                                                                                                                                                                                                                                                      \
+        counter += SHAPE_OUT;                                                                                                                                                                                                                                                                                                                                                                                  \
+    } while (0)
+
 // mirror the TensorFlow model as static model here:
 
-RTNeural::ModelT<float, IN_SHAPE, IN_SHAPE,
-    RTNeural::DenseT<float, IN_SHAPE, SHAPE_L_1>,
-    RTNeural::ReLuActivationT<float, SHAPE_L_1>,
-    RTNeural::DenseT<float, SHAPE_L_1, SHAPE_L_2>,
-    RTNeural::ReLuActivationT<float, SHAPE_L_2>,
-    RTNeural::DenseT<float, SHAPE_L_2, SHAPE_L_3>,
-    RTNeural::ReLuActivationT<float, SHAPE_L_3>,
-    RTNeural::DenseT<float, SHAPE_L_3, SHAPE_L_4>,
-    RTNeural::ReLuActivationT<float, SHAPE_L_4>,
-    RTNeural::DenseT<float, SHAPE_L_4, SHAPE_L_5>,
-    RTNeural::ReLuActivationT<float, SHAPE_L_5>,
-    RTNeural::DenseT<float, SHAPE_L_5, SHAPE_L_6>> taunet;
+//clang-format off
+
+RTNeural::ModelT<float, IN_SHAPE, IN_SHAPE, RTNeural::DenseT<float, IN_SHAPE, SHAPE_L_1>, RTNeural::ReLuActivationT<float, SHAPE_L_1>, RTNeural::DenseT<float, SHAPE_L_1, SHAPE_L_2>, RTNeural::ReLuActivationT<float, SHAPE_L_2>, RTNeural::DenseT<float, SHAPE_L_2, SHAPE_L_3>, RTNeural::ReLuActivationT<float, SHAPE_L_3>, RTNeural::DenseT<float, SHAPE_L_3, SHAPE_L_4>,
+                 RTNeural::ReLuActivationT<float, SHAPE_L_4>, RTNeural::DenseT<float, SHAPE_L_4, SHAPE_L_5>, RTNeural::ReLuActivationT<float, SHAPE_L_5>, RTNeural::DenseT<float, SHAPE_L_5, SHAPE_L_6>>
+    taunet;
+
+//clang-format on
 
 float ATTACK_T1 = 2;
 float SUSTAIN_T1 = 2;
 
-float DSY_QSPI_BSS weights_l1_buffer[SHAPE_L_1][IN_SHAPE];
-float DSY_QSPI_BSS bias_l1_buffer[SHAPE_L_1];
+uint32_t DSY_QSPI_BSS weigthsQSPI[1000000];
 
-float DSY_QSPI_BSS weights_l2_buffer[SHAPE_L_2][SHAPE_L_1];
-float DSY_QSPI_BSS bias_l2_buffer[SHAPE_L_2];
+// uint32_t DSY_QSPI_BSS weights_l1_buffer[SHAPE_L_1][IN_SHAPE];
+// uint32_t DSY_QSPI_BSS bias_l1_buffer[SHAPE_L_1];
 
-float DSY_QSPI_BSS weights_l3_buffer[SHAPE_L_3][SHAPE_L_2];
-float DSY_QSPI_BSS bias_l3_buffer[SHAPE_L_3];
+// uint32_t DSY_QSPI_BSS weights_l2_buffer[SHAPE_L_2][SHAPE_L_1];
+// uint32_t DSY_QSPI_BSS bias_l2_buffer[SHAPE_L_2];
 
-float DSY_QSPI_BSS weights_l4_buffer[SHAPE_L_4][SHAPE_L_3];
-float DSY_QSPI_BSS bias_l4_buffer[SHAPE_L_4];
+// uint32_t DSY_QSPI_BSS weights_l3_buffer[SHAPE_L_3][SHAPE_L_2];
+// uint32_t DSY_QSPI_BSS bias_l3_buffer[SHAPE_L_3];
 
-float DSY_QSPI_BSS weights_l5_buffer[SHAPE_L_5][SHAPE_L_4];
-float DSY_QSPI_BSS bias_l5_buffer[SHAPE_L_5];
+// uint32_t DSY_QSPI_BSS weights_l4_buffer[SHAPE_L_4][SHAPE_L_3];
+// uint32_t DSY_QSPI_BSS bias_l4_buffer[SHAPE_L_4];
 
-float DSY_QSPI_BSS weights_l6_buffer[SHAPE_L_6][SHAPE_L_5];
-float DSY_QSPI_BSS bias_l6_buffer[SHAPE_L_6];
+// uint32_t DSY_QSPI_BSS weights_l5_buffer[SHAPE_L_5][SHAPE_L_4];
+// uint32_t DSY_QSPI_BSS bias_l5_buffer[SHAPE_L_5];
+
+// uint32_t DSY_QSPI_BSS weights_l6_buffer[SHAPE_L_6][SHAPE_L_5];
+// uint32_t DSY_QSPI_BSS bias_l6_buffer[SHAPE_L_6];
+
+float bias_l1_buffer[SHAPE_L_1];
+float bias_l2_buffer[SHAPE_L_2];
+float __attribute__((section(".sdram_bss"))) bias_l3_buffer[SHAPE_L_3];
+float __attribute__((section(".sdram_bss"))) bias_l4_buffer[SHAPE_L_4];
+float __attribute__((section(".sdram_bss"))) bias_l5_buffer[SHAPE_L_5];
+float __attribute__((section(".sdram_bss"))) bias_l6_buffer[SHAPE_L_6];
 
 std::vector<std::vector<float>> weights_l1;
 std::vector<std::vector<float>> weights_l2;
@@ -51,71 +81,86 @@ float probeA = 0;
 float probeB = 0;
 float probeC = 0;
 
-void aiInit(void){
-    auto& layer_1 = taunet.get<0>();
-    auto& layer_2 = taunet.get<2>();
-    auto& layer_3 = taunet.get<4>();
-    auto& layer_4 = taunet.get<6>();
-    auto& layer_5 = taunet.get<8>();
-    auto& layer_6 = taunet.get<10>();
+void aiInit(void)
+{
+    auto &layer_1 = taunet.get<0>();
+    auto &layer_2 = taunet.get<2>();
+    auto &layer_3 = taunet.get<4>();
+    auto &layer_4 = taunet.get<6>();
+    auto &layer_5 = taunet.get<8>();
+    auto &layer_6 = taunet.get<10>();
 
-    for(int i = 0; i < SHAPE_L_1;i++) //load weights_l1 from flash
-    {
-        std::vector<float> row;
-        for(int j = 0; j < IN_SHAPE;j++)
-        {
-            row.push_back(weights_l1_buffer[i][j]);
-        }
-        weights_l1.push_back(row); 
-    }
+    uint32_t counterQSPILoading = 0;
 
+    LOAD_WEIGHTS(IN_SHAPE, SHAPE_L_1, weights_l1, weigthsQSPI, counterQSPILoading);
     layer_1.setWeights(weights_l1);
-    layer_1.setBias(bias_l1_buffer); //TODO: MAYBE WE NEED A IN BETWEEN VARIABLE???
-    //TODO: LOAD BIAS STUFF
 
-    //END OF LAYER 1 LOADING
+    LOAD_BIAS(SHAPE_L_1, bias_l1_buffer, weigthsQSPI, counterQSPILoading);
+    layer_1.setBias(bias_l1_buffer);
+    //*(float *)&sourcePointer[i * SHAPE_IN + j]
 
-    for(int i = 0; i < SHAPE_L_2;i++) //load weights_l2 from flash
-    {
-        std::vector<float> row;
-        for(int j = 0; j < SHAPE_L_1;j++)
-        {
-            row.push_back(weights_l2_buffer[i][j]);
-        }
-        weights_l2.push_back(row); 
-    }
-
+    LOAD_WEIGHTS(SHAPE_L_1, SHAPE_L_2, weights_l2, weigthsQSPI, counterQSPILoading);
     layer_2.setWeights(weights_l2);
 
-    //END OF LAYER 1 LOADING
+    LOAD_BIAS(SHAPE_L_2, bias_l2_buffer, weigthsQSPI, counterQSPILoading);
+    layer_2.setBias((const float *)bias_l2_buffer);
 
+    // memcpy(bias_l1_buffer, weigthsQSPI + counterQSPILoading, SHAPE_L_1 * 4);
+    // counterQSPILoading += SHAPE_L_1;
+    // layer_1.setBias((const float *)bias_l1_buffer);
 
-    probeA = weights_l2[0][0];
-    probeB = weights_l2[0][1];
-    probeC = weights_l2[SHAPE_L_2-1][SHAPE_L_1-1];
-    
-    /*
-    layer_2.setWeights(weights_l2);
-    layer_3.setWeights(weights_l3);
-    layer_4.setWeights(weights_l4);
-    layer_5.setWeights(weights_l5);
-    layer_6.setWeights(weights_l6);
+    // LOAD_WEIGHTS(SHAPE_L_1, SHAPE_L_2, weights_l2, weights_l2_buffer);
+    // layer_2.setWeights(weights_l2);
+    // layer_2.setBias((const float*)bias_l2_buffer);
 
-    layer_1.setBias(bias_l1);
-    layer_2.setBias(bias_l2);
-    layer_3.setBias(bias_l3);
-    layer_4.setBias(bias_l4);
-    layer_5.setBias(bias_l5);
-    layer_6.setBias(bias_l6);
-    taunet.reset();*/
+    // LOAD_WEIGHTS(SHAPE_L_2, SHAPE_L_3, weights_l3, weights_l3_buffer);
+    // layer_3.setWeights(weights_l3);
+    // layer_3.setBias((const float*)bias_l3_buffer);
+
+    // LOAD_WEIGHTS(SHAPE_L_3, SHAPE_L_4, weights_l4, weights_l4_buffer);
+    // layer_4.setWeights(weights_l4);
+    // layer_4.setBias((const float*)bias_l4_buffer);
+
+    // LOAD_WEIGHTS(SHAPE_L_4, SHAPE_L_5, weights_l5, weights_l5_buffer);
+    // layer_5.setWeights(weights_l5);
+    // layer_5.setBias((const float*)bias_l5_buffer);
+
+    // LOAD_WEIGHTS(SHAPE_L_5, SHAPE_L_6, weights_l6, weights_l6_buffer);
+    // layer_6.setWeights(weights_l6);
+    // layer_6.setBias((const float*)bias_l6_buffer);
+
+    probeB = bias_l2_buffer[0];
+    // probeB = weights_l2[1][0];
+    // probeC = weights_l2[SHAPE_L_2 - 1][SHAPE_L_1 - 1];
+
+    // layer_2.setWeights(weights_l2);
+    // layer_3.setWeights(weights_l3);
+    // layer_4.setWeights(weights_l4);
+    // layer_5.setWeights(weights_l5);
+    // layer_6.setWeights(weights_l6);
+
+    // layer_1.setBias(bias_l1);
+    // layer_2.setBias(bias_l2);
+    // layer_3.setBias(bias_l3);
+    // layer_4.setBias(bias_l4);
+    // layer_5.setBias(bias_l5);
+    // layer_6.setBias(bias_l6);
+    // taunet.reset();
 }
 
-void aiRun(float* input){
-    taunet.forward((float*)input);
+void aiRun(float *input)
+{
+    taunet.forward((float *)input);
     ATTACK_T1 = taunet.getOutputs()[0];
     SUSTAIN_T1 = taunet.getOutputs()[1];
 }
 
-float aiGetATTACK_T1(void) { return ATTACK_T1; }
+float aiGetATTACK_T1(void)
+{
+    return ATTACK_T1;
+}
 
-float aiGetSUSTAIN_T1(void) { return SUSTAIN_T1; }
+float aiGetSUSTAIN_T1(void)
+{
+    return SUSTAIN_T1;
+}
--- src/qspiFlashLoader/QSPI.cpp
+++ src/qspiFlashLoader/QSPI.cpp
@@ -5,6 +5,22 @@ using namespace daisy;
 
 DaisySeed hw;
 
+// #define FLAT_INSERT(COUNTER, SHAPE_IN, SHAPE_OUT, target, source)                                                                                                                                                                                                                                                                                                                                              \
+//     do                                                                                                                                                                                                                                                                                                                                                                                                         \
+//     {
+
+// for (int i = 0; i < SHAPE_OUT; i++)
+// {
+//     std::vector<float> row;
+//     for (int j = 0; j < SHAPE_IN; j++)
+//     {
+//         row.push_back((float)source[i][j]);
+//     }
+//     target.push_back(row);
+// }
+// }
+// while (0)
+
 #define IN_SHAPE 13
 
 #define SHAPE_L_1 64
@@ -14,23 +30,25 @@ DaisySeed hw;
 #define SHAPE_L_5 4
 #define SHAPE_L_6 2
 
-uint64_t DSY_QSPI_BSS weights_l1_buffer[SHAPE_L_1][IN_SHAPE];
-uint64_t DSY_QSPI_BSS bias_l1_buffer[SHAPE_L_1];
+uint32_t DSY_QSPI_BSS weights_l1_buffer[SHAPE_L_1][IN_SHAPE];
+// uint32_t DSY_QSPI_BSS bias_l1_buffer[SHAPE_L_1];
 
-uint64_t DSY_QSPI_BSS weights_l2_buffer[SHAPE_L_2][SHAPE_L_1];
-uint64_t DSY_QSPI_BSS bias_l2_buffer[SHAPE_L_2];
+// uint32_t DSY_QSPI_BSS weights_l2_buffer[SHAPE_L_2][SHAPE_L_1];
+// uint32_t DSY_QSPI_BSS bias_l2_buffer[SHAPE_L_2];
 
-uint64_t DSY_QSPI_BSS weights_l3_buffer[SHAPE_L_3][SHAPE_L_2];
-uint64_t DSY_QSPI_BSS bias_l3_buffer[SHAPE_L_3];
+// uint32_t DSY_QSPI_BSS weights_l3_buffer[SHAPE_L_3][SHAPE_L_2];
+// uint32_t DSY_QSPI_BSS bias_l3_buffer[SHAPE_L_3];
 
-uint64_t DSY_QSPI_BSS weights_l4_buffer[SHAPE_L_4][SHAPE_L_3];
-uint64_t DSY_QSPI_BSS bias_l4_buffer[SHAPE_L_4];
+// uint32_t DSY_QSPI_BSS weights_l4_buffer[SHAPE_L_4][SHAPE_L_3];
+// uint32_t DSY_QSPI_BSS bias_l4_buffer[SHAPE_L_4];
 
-uint64_t DSY_QSPI_BSS weights_l5_buffer[SHAPE_L_5][SHAPE_L_4];
-uint64_t DSY_QSPI_BSS bias_l5_buffer[SHAPE_L_5];
+// uint32_t DSY_QSPI_BSS weights_l5_buffer[SHAPE_L_5][SHAPE_L_4];
+// uint32_t DSY_QSPI_BSS bias_l5_buffer[SHAPE_L_5];
 
-uint64_t DSY_QSPI_BSS weights_l6_buffer[SHAPE_L_6][SHAPE_L_5];
-uint64_t DSY_QSPI_BSS bias_l6_buffer[SHAPE_L_6];
+// uint32_t DSY_QSPI_BSS weights_l6_buffer[SHAPE_L_6][SHAPE_L_5];
+// uint32_t DSY_QSPI_BSS bias_l6_buffer[SHAPE_L_6];
+
+uint32_t *intermediateBuffer;
 
 int main(void)
 {
@@ -41,92 +59,144 @@ int main(void)
     size_t size = sizeof(weights_l1);
     size_t address = (size_t)weights_l1_buffer;
 
-    hw.qspi.Erase(address, address + size);
-    hw.qspi.Write(address, size, (uint8_t *)weights_l1);
+    // clang-format off
+    size_t totalSize =
+    sizeof(weights_l1) + sizeof(bias_l1) +
+    sizeof(weights_l2) + sizeof(bias_l2) +
+    sizeof(weights_l3) + sizeof(bias_l3) +
+    sizeof(weights_l4) + sizeof(bias_l4) +
+    sizeof(weights_l5) + sizeof(bias_l5) +
+    sizeof(weights_l6) + sizeof(bias_l6);
+    // clang-format on
+    intermediateBuffer = (uint32_t *)malloc(totalSize);
 
-    size = sizeof(bias_l1);
-    address = (size_t)bias_l1_buffer;
+    uint32_t intermediateBufferCounter = 0;
 
-    hw.qspi.Erase(address, address + size);
+    memcpy(&intermediateBuffer[intermediateBufferCounter], weights_l1, sizeof(weights_l1));
+    intermediateBufferCounter += sizeof(weights_l1) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], bias_l1, sizeof(bias_l1));
+    intermediateBufferCounter += sizeof(bias_l1) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], weights_l2, sizeof(weights_l2));
+    intermediateBufferCounter += sizeof(weights_l2) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], bias_l2, sizeof(bias_l2));
+    intermediateBufferCounter += sizeof(bias_l2) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], weights_l3, sizeof(weights_l3));
+    intermediateBufferCounter += sizeof(weights_l3) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], bias_l3, sizeof(bias_l3));
+    intermediateBufferCounter += sizeof(bias_l3) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], weights_l4, sizeof(weights_l4));
+    intermediateBufferCounter += sizeof(weights_l4) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], bias_l4, sizeof(bias_l4));
+    intermediateBufferCounter += sizeof(bias_l4) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], weights_l5, sizeof(weights_l5));
+    intermediateBufferCounter += sizeof(weights_l5) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], bias_l5, sizeof(bias_l5));
+    intermediateBufferCounter += sizeof(bias_l5) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], weights_l6, sizeof(weights_l6));
+    intermediateBufferCounter += sizeof(weights_l6) / 4;
+
+    memcpy(&intermediateBuffer[intermediateBufferCounter], bias_l6, sizeof(bias_l6));
+    intermediateBufferCounter += sizeof(bias_l6) / 4;
+
+    hw.qspi.Erase(address, address + totalSize);
+
+    hw.qspi.Write(address, totalSize, (uint8_t *)intermediateBuffer);
+
+    //
+
+    /*size = sizeof(bias_l1);
+    address = 0x90003898; //(size_t)bias_l1_buffer;
+
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)bias_l1);
 
     // WEIGHTS FROM LAYER 1 END
 
     size = sizeof(weights_l2);
-    address = (size_t)weights_l2_buffer;
+    address = 0x90001898; //(size_t)weights_l2_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)weights_l2);
 
     size = sizeof(bias_l2);
-    address = (size_t)bias_l2_buffer;
+    address = 0x90001818; //(size_t)bias_l2_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)bias_l2);
 
     // WEIGHTS FROM LAYER 2 END
 
     size = sizeof(weights_l3);
-    address = (size_t)weights_l3_buffer;
+    address = 0x90001018; //(size_t)weights_l3_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)weights_l3);
 
     size = sizeof(bias_l3);
-    address = (size_t)bias_l3_buffer;
+    address = 0x90000fd8; //(size_t)bias_l3_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)bias_l3);
 
     // WEIGHTS FROM LAYER 3 END
 
     size = sizeof(weights_l4);
-    address = (size_t)weights_l4_buffer;
+    address = 0x90000dd8; //(size_t)weights_l4_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)weights_l4);
 
     size = sizeof(bias_l4);
-    address = (size_t)bias_l4_buffer;
+    address = 0x90000db8; //(size_t)bias_l4_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)bias_l4);
 
     // WEIGHTS FROM LAYER 4 END
 
     size = sizeof(weights_l5);
-    address = (size_t)weights_l5_buffer;
+    address = 0x90000d38; //(size_t)weights_l5_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)weights_l5);
 
     size = sizeof(bias_l5);
-    address = (size_t)bias_l5_buffer;
+    address = 0x90000d28; //(size_t)bias_l5_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)bias_l5);
 
     // WEIGHTS FROM LAYER 5 END
 
     size = sizeof(weights_l6);
-    address = (size_t)weights_l6_buffer;
+    address = 0x90000d08; //(size_t) weights_l6_buffer;
 
-    hw.qspi.Erase(address, address + size);
+    // hw.qspi.Erase(address, address + size);
     hw.qspi.Write(address, size, (uint8_t *)weights_l6);
 
     size = sizeof(bias_l6);
-    address = (size_t)bias_l6_buffer;
+    address = 0x90000d00; //(size_t)bias_l6_buffer;
 
-    hw.qspi.Erase(address, address + size);
-    hw.qspi.Write(address, size, (uint8_t *)bias_l6);
+    // hw.qspi.Erase(address, address + size);
+    hw.qspi.Write(address, size, (uint8_t *)bias_l6);*/
 
     // WEIGHTS FROM LAYER 6 END
 
     while (1)
     {
         hw.SetLed(true);
-        System::Delay(500);
+        System::Delay(1000);
         hw.SetLed(false);
-        System::Delay(500);
+        System::Delay(1000);
     }
 }
